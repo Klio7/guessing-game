0a7597762a76d80e54dfd2d6723e5d04681882f4
Anna Odintsova <Klio7@mail.ru>
2023-02-27 17:28:07 +0300

    feat: implement task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+  _min = 0;
+  _max = 0;
+  _guess = 0;
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this._min = min;
+    this._max = max;
+  }
 
-    }
+  guess() {
+    this._guess = Math.round((this._max + this._min) / 2);
 
-    guess() {
+    return this._guess;
+  }
 
-    }
+  lower() {
+    this._max = this._guess;
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this._min = this._guess;
+  }
 }
 
 module.exports = GuessingGame;
